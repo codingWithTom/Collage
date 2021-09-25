5cf37043847369e33c5dd949bd99586e44eb96fa
Tomas Trujillo <t.trujillo138@gmail.com>
2021-09-25 07:33:35 -0700

    Initial Commit

---
+++ Shared/CollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollageApp.swift
+//  Shared
+//
+//  Created by Tomas Trujillo on 2021-09-25.
+//
+
+import SwiftUI
+
+@main
+struct CollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
